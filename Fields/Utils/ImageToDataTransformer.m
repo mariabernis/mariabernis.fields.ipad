9671571dfb03a46f0861684b189e4138d89c7745
Maria Bernis <mariabernis.clavijo@gmail.com>
2014-11-26 17:56:32 +0100

    More model changes. Pre mogen.

---
+++ Fields/Utils/ImageToDataTransformer.m
@@ -0,0 +1,27 @@
+
+#import "ImageToDataTransformer.h"
+#import <UIKit/UIKit.h>
+
+@implementation ImageToDataTransformer
+
++ (BOOL)allowsReverseTransformation {
+	return YES;
+}
+
++ (Class)transformedValueClass {
+	return [NSData class];
+}
+
+
+- (id)transformedValue:(id)value {
+	NSData *data = UIImagePNGRepresentation(value);
+	return data;
+}
+
+
+- (id)reverseTransformedValue:(id)value {
+	UIImage *uiImage = [[UIImage alloc] initWithData:value];
+	return uiImage;
+}
+
+@end
